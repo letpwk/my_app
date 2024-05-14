e3afcabb68d77496285c699409365e2894320c2f
letpwk <leticiass.pw@gmail.com>
2024-05-14 08:58:52 -0400

    atualizando dnv aa

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,32 @@ import 'package:flutter/material.dart'; //importa um pacote (esse é do material
 
 void main() {
   runApp(const MeuApp()); //roda um app
-  //
-  /*
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Wave to Earth'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello World', 
-            style: TextStyle(fontSize: 50, color: Colors.lightBlue),
-        ),
-      ),
-    ), 
-    ),
-  );*/
 }
 
 class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
+  const MeuApp({Key? key}) : super(key: key);
 
   @override //sobreescrevendo a classe pai
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Poesias - Adamastora Pitaca, feat. Adamastor Pitaco'),
-        ),
-        body: Center(
-          child: Text(
-            'Vou te contar uma coisa...',
-            style: TextStyle(fontSize: 50, color: Colors.red),
+      title: 'Wave to Earth',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
           ),
-        ),
+          Container(
+            color: Colors.blue,
+            width: 50,
+            height: 50,
+          ),
+        ],
       ),
     );
   }
