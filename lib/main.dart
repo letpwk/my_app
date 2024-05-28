1cf5f31bdee09506730b5ebed36d264176aa24ec
Let <leticiass.pw@gmail.com>
2024-05-27 23:58:25 -0300

    github n to te entendendo

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,84 @@ class MeuApp extends StatelessWidget {
   @override //sobreescrevendo a classe pai
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wave to Earth',
+      title: 'App da Let',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
+      home: Container(
+        color: Colors.black,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.purple,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.blueGrey,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.greenAccent,
+                    height: 50,
+                    width: 50,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text(
+                  'Diamante Amarelo',
+                  style: TextStyle(color: Colors.black, fontSize: 28),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('Você apertou o botão');
+                  },
+                  child: Text('Aperte o botão!'))
+            ]
           ),
-          Container(
-            color: Colors.blue,
-            width: 50,
-            height: 50,
-          ),
-        ],
       ),
     );
   }
