71712f524c53333fe2f8d31c57691aff878aad4e
letpwk <160621638+letpwk@users.noreply.github.com>
2024-06-11 12:11:24 -0400

    atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,73 @@ class MeuApp extends StatelessWidget {
   @override //sobreescrevendo a classe pai
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App da Let',
+      title: 'Wave to Earth',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: ,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
+        ),
+        body: ListView(
+          children: [
+            Task('Aprender Flutter no café da manhã comendo sucrilhos'),
+            Task('Andar de Bike'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Container(
+                  width: 200,
+                    child: Text(
+                  nome,
+                  style: TextStyle(
+                    fontSize: 24,
+                    overflow: TextOverflow.ellipsis
+                  ),
+                )),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
